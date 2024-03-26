f5c2c0856fddbf9d7b0eb3cba0955ca057f61603
Lucas <lucasclaudinocc@gmail.com>
2024-03-26 11:07:03 -0300

    Adding base structure for next.js project

---
+++ src/app/pages/MainPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = () => {
+  return (
+    <div className="flex justify-center items-center h-[100svh]">
+      TESTE
+    </div>
+  )
+}
+
+export default MainPage
