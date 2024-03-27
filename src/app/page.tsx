606d14346f1de7933297b73e027d2e9237fdda72
Lucas <lucasclaudinocc@gmail.com>
2024-03-27 12:46:12 -0300

    Creating modal to manage languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import MainPage from "./pages/MainPage";
+import { Navbar } from "./common/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <MainPage />
     </>
   );
